c03873985f2a9cd84050dab374938a532f56565e
yeeunni <kye9142@naver.com>
2024-11-08 13:51:57 +0900

    project setting of spring boot

---
+++ src/main/java/mandooparty/mandoo/MandooApplication.java
@@ -0,0 +1,13 @@
+package mandooparty.mandoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mandooparty/mandoo/MandooApplicationTests.java
@@ -0,0 +1,13 @@
+package mandooparty.mandoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
